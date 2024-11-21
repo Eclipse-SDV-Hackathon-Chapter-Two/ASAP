544f2ce5c2caf25df32b4ea8709e17e379069297
Bodenstein <Philipp.Bodenstein@asap.de>
2024-11-21 11:28:59 +0100

    Add Traffic Sign confidences

--- apps/speed_limit_assist/speed_limit_assist.py
+++ apps/speed_limit_assist/speed_limit_assist.py
@@ -18,8 +18,9 @@ import ecal.core.core as ecal_core
 from ecal.core.subscriber import StringSubscriber
 
 class SpeedLimitAssist:
-    def __init__(self, limit, vel_mps):
+    def __init__(self, limit, limit_confidence, vel_mps):
         self.limit = limit
+        self.limit_confidence = limit_confidence
         self.vel_mps = vel_mps
     
     
@@ -27,8 +28,8 @@ class SpeedLimitAssist:
         
         if self.vel_mps > 0.0 and self.limit > 0.0:
             exceeded_speed = self.vel_mps - self.limit
-            if exceeded_speed > 0.0:
-                warning = "You are exceeding the current speed limit= " + str(self.limit*3.6) + " by " + str(exceeded_speed*3.6) +  "km/h"
+            if exceeded_speed > 0.0 and self.limit_confidence > 0.5:
+                warning = "You are exceeding the current speed limit= " + str(self.limit*3.6) + " by " + str(exceeded_speed*3.6) +  "km/h with a confidence of " + str(self.limit_confidence)
                 logger.warning(warning)
 
 logger = logging.getLogger("speed_limit_assist")
@@ -55,7 +56,6 @@ def ego_callback(topic_name, msg, time):
         logger.error(f"Error: Could not decode message: '{msg}'")
     except Exception as e:
         logger.error(f"Error: {e}")
-    pass
 
 
 # Callback for receiving messages
@@ -63,33 +63,39 @@ def speed_limit_callback(topic_name, msg, time):
     try:
         json_msg = json.loads(msg)
         class_ids = json_msg["class_ids"]
+        confidences = json_msg["confidences"]
         current_speed_limit = 0.0
-        for class_id in class_ids:
-            if class_id == 4:
-                current_speed_limit = kmh2mps(10)
-            elif class_id == 5:
-                current_speed_limit = kmh2mps(100)
-            elif class_id == 6:
-                current_speed_limit = kmh2mps(130)
-            elif class_id == 7:
-                current_speed_limit = kmh2mps(20)
-            elif class_id == 8:
-                current_speed_limit = kmh2mps(30)
-            elif class_id == 9:
-                current_speed_limit = kmh2mps(40)
-            elif class_id == 10:
-                current_speed_limit = kmh2mps(5)
-            elif class_id == 11:
-                current_speed_limit = kmh2mps(50)
-            elif class_id == 12:
-                current_speed_limit = kmh2mps(60)
-            elif class_id == 13:
-                current_speed_limit = kmh2mps(70)
-            elif class_id == 14:
-                current_speed_limit = kmh2mps(80)
-            elif class_id == 15:
-                current_speed_limit = kmh2mps(90)
-            speedLimitAssist.limit = current_speed_limit
+        if len(class_ids) == len(confidences):
+            for i in range(0, len(class_ids)):
+                class_id = class_ids[i]
+                confidence = confidences[i]
+
+                if class_id == 4:
+                    current_speed_limit = kmh2mps(10)
+                elif class_id == 5:
+                    current_speed_limit = kmh2mps(100)
+                elif class_id == 6:
+                    current_speed_limit = kmh2mps(130)
+                elif class_id == 7:
+                    current_speed_limit = kmh2mps(20)
+                elif class_id == 8:
+                    current_speed_limit = kmh2mps(30)
+                elif class_id == 9:
+                    current_speed_limit = kmh2mps(40)
+                elif class_id == 10:
+                    current_speed_limit = kmh2mps(5)
+                elif class_id == 11:
+                    current_speed_limit = kmh2mps(50)
+                elif class_id == 12:
+                    current_speed_limit = kmh2mps(60)
+                elif class_id == 13:
+                    current_speed_limit = kmh2mps(70)
+                elif class_id == 14:
+                    current_speed_limit = kmh2mps(80)
+                elif class_id == 15:
+                    current_speed_limit = kmh2mps(90)
+                speedLimitAssist.limit = current_speed_limit
+                speedLimitAssist.limit_confidence = confidence
         #print(f"Received speed_limit: {current_speed_limit}")
     except json.JSONDecodeError:
         logger.error(f"Error: Could not decode message: '{msg}'")
@@ -103,7 +109,7 @@ if __name__ == "__main__":
     ecal_core.initialize(sys.argv, "Speed Limit Assist")
 
     # Initialize SpeedLimitAssist
-    speedLimitAssist = SpeedLimitAssist(0.0, 0.0)
+    speedLimitAssist = SpeedLimitAssist(0.0, 0.0, 0.0)
 
     # Create a subscriber that listens on the "traffic_sign_detection"
     tsd_sub = StringSubscriber("traffic_sign_detection")
